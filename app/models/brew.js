7c7b03af749212ca8fb6c87d3bfd8575945fef3d
taharon <taharon1990@gmail.com>
2020-05-11 17:15:52 -0400

    Models finished
    Created brew & comment model. Brew currently has reference to upload model, for stretch goal, but is unused

---
+++ app/models/brew.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const comment = require('./comment')
+
+const brewSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  style: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  steep: {
+    type: Array
+  },
+  boil: {
+    type: Array
+  },
+  post: {
+    type: Array
+  },
+  tastingNotes: String,
+  // pictures: [picture.schema],
+  comments: [comment.schema]
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Brew', brewSchema)
---
+++ app/models/comment.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+  body: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = {
+  model: mongoose.model('Comment', commentSchema),
+  schema: commentSchema
+}
